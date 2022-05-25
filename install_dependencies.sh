aee30403d6ef215a551f60dba46714ccc56744f8
josibake <josibake@protonmail.com>
2022-05-25 15:47:48 +0200

    install bcc directly

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,36 +1,41 @@
 #!/usr/bin/env bash
 apt-get update
 
-echo "-------------------------------"
-echo "| installing bcc dependencies |"
-echo "-------------------------------"
+# echo "-------------------------------"
+# echo "| installing bcc dependencies |"
+# echo "-------------------------------"
+# 
+# # install bcc build dependencies
+# apt-get install -y bison build-essential cmake flex git libedit-dev libllvm11 llvm-11-dev libclang-11-dev zlib1g-dev libelf-dev libfl-dev python3-distutils luajit luajit-5.1-dev
 
-# install bcc build dependencies
-apt-get install -y bison build-essential cmake flex git libedit-dev libllvm11 llvm-11-dev libclang-11-dev zlib1g-dev libelf-dev libfl-dev python3-distutils luajit luajit-5.1-dev
 
+# echo "----------------"
+# echo "| building bcc |"
+# echo "----------------"
+# 
+# # build bcc. this is necessary to get around a bunch of warnings
+# git clone https://github.com/iovisor/bcc.git
+# mkdir bcc/build; cd bcc/build
+# cmake ..
+# make
+# sudo make install
+# cmake -DPYTHON_CMD=python3 .. # build python3 binding
+# pushd src/python/
+# make
+# sudo make install
+# popd
+# 
+# echo "----------------------------------------"
+# echo "| installing bitcoin core dependencies |"
+# echo "----------------------------------------"
 
-echo "----------------"
-echo "| building bcc |"
-echo "----------------"
+echo "---------------"
+echo "| install bcc |"
+echo "---------------"
 
-# build bcc. this is necessary to get around a bunch of warnings
-git clone https://github.com/iovisor/bcc.git
-mkdir bcc/build; cd bcc/build
-cmake ..
-make
-sudo make install
-cmake -DPYTHON_CMD=python3 .. # build python3 binding
-pushd src/python/
-make
-sudo make install
-popd
-
-echo "----------------------------------------"
-echo "| installing bitcoin core dependencies |"
-echo "----------------------------------------"
+apt-get install bpfcc-tools linux-headers-$(uname -r)
 
 # install build dependencies for bitcoin core
-cd 
 apt-get install build-essential libtool autotools-dev automake pkg-config bsdmainutils python3 libevent-dev libboost-dev libsqlite3-dev systemtap-sdt-dev
 
 # install pip and gitPython
